38a367e6decdf3aec339bdd77d431ceab8b52b6d
seanjkr <seankoppreddy@gmail.com>
2020-11-28 16:21:17 -0600

    Added material for styling, working on adding multiple to-do list functionality.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,77 @@
 <template>
 
-  <div>
+  <div class = "container">
 
-    <input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo">
-    
-    <ul class = "todos">
+    <md-toolbar class = "main-nav md-medium">
 
-      <li v-for = "todo in todos" :key = "todo.id" >
+      <div class="md-toolbar-row">
 
-        <div class = "todoView">
-
-          <input type = "checkbox" v-model = "todo.completed" />
-
-          <span :class = "{ editing : editedTodo === todo }" @dblclick = "editTodo( todo )" v-show = "editedTodo !== todo">
-            {{ todo.label }}
-          </span>
-
-          <input class = "editing" v-model = "todo.label" v-show = "editedTodo == todo"@keyup.esc = "noEdit( todo )" @keyup.enter = "completeEdit( todo )" />
-
-          <button @click = "removeTodo( todo )"> Delete </button>
-
+        <div class="md-toolbar-section-start">
+          <h3 class="md-display-1">Doin Stuff!</h3>
         </div>
 
-      </li>
-    </ul>
+        <md-button class="md-icon-button">
+          <md-icon>more_vert</md-icon>
+        </md-button>
 
+      </div>
+
+      <div class="md-toolbar-row">
+        <h3 class="md-caption"> The one stop shop for all your To-Do needs </h3>
+      </div>
+
+    </md-toolbar>
+
+    <header class = "todos-header">
+      <h1 class = "md-display-1"> To-Do Lists! </h1>
+    </header>
+
+    <div class = "todo-main md-layout">
+
+      <md-card class = "todoList-card md-layout-item md-size-30" v-for = "todoList in todoLists" :key = "todoList.id" >
+
+        <md-card-header>
+
+          <md-field>
+            <md-input class = "add-todo" v-model = "currentTodo" @keydown.enter = "addTodo( todoList )" placeholder = "Add a todo" />
+          </md-field>
+      
+        </md-card-header>
+
+        <md-card-content>
+
+          <md-list>
+
+            <md-list-item v-for = "todo in todoList.todos" :key = "todo.id" >
+
+              <input type = "checkbox" v-model = "todo.completed"/>
+
+              <span :class = "{ editing : editedTodo === todo }" class = "todo-label" v-on:click = "editTodo( todo )" v-show = "editedTodo !== todo">
+                {{ todo.label }}
+              </span>
+
+              <input class = "editing" v-model = "todo.label" v-show = "editedTodo == todo" @keyup.esc = "noEdit( todo )" @keyup.enter = "completeEdit( todo )"/>
+
+              <md-button @click = "removeTodo( todo )"> Delete </md-button>
+
+            </md-list-item>
+
+          </md-list>
+
+        </md-card-content>
+
+        <md-card-actions>
+          <md-button @click = "removeTodoList( todoList )"> Delete </md-button>
+        </md-card-actions>
+
+      </md-card>
+
+      <md-button class="md-icon-button md-layout-item" v-on:click = "addTodoList()" >
+        <md-icon>add</md-icon>
+      </md-button>
+
+    </div>
+  
   </div>
 
 </template>
@@ -34,15 +81,24 @@
   export default {
     data() {
       return {
-        todos : [],
+        todoLists : [ [] , [] ],
+        currentTodoList : '',
         currentTodo : '',
         editedTodo : null,
         originalTodoLabel : ''
       };
     },
     methods : {
-      addTodo() {
-        this.todos.push({ id : this.todos.length, label : this.currentTodo, completed : false });
+      addTodoList() {
+        this.todoLists.push({ id : this.todoLists.length , todos : [ { label : "butts" } , { label : "buttz" } ] });
+        this.currentTodoList = this.todoList;
+      },
+      removeTodoList( todoList ) {
+        var index = this.todoLists.indexOf( todoList );
+        this.todoLists.splice( index , 1 );
+      },
+      addTodo( todoList ) {
+        todoList.push( { id : todoList.length , todos : [ { label : this.currentTodo , complete : false }] });
         this.currentTodo = '';
       },
       removeTodo( todo ) {
@@ -74,16 +130,40 @@
 
 <style>
 
-ul {
-  list-style-type : none;
+.container {
+  text-align : center;
+  align-items : center;
 }
 
-.todoView {
+.main-nav {
+  color : white;
+  background-color : black;
+}
 
+.todos-header {
+  margin-top : 40px;
+}
+
+.todo-main {
+  margin-top : 30px;
+  text-align : center;
+}
+
+.todoList-card {
+  width : 80rem;
+  margin : 10px;
+}
+
+.todo-label {
+  max-height : 80px;
+}
+
+.add-todo {
+  text-align : center;
 }
 
 .editing {
-
+  width : 40%;
 }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import { MdField } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
+import { MdList } from 'vue-material/dist/components';
+import { MdButton } from 'vue-material/dist/components';
+import { MdCard } from 'vue-material/dist/components';
+import { MdIcon } from 'vue-material/dist/components';
+import { MdToolbar } from 'vue-material/dist/components';
 
 Vue.use( MdField );
+Vue.use( MdList );
+Vue.use( MdButton );
+Vue.use( MdCard );
+Vue.use( MdIcon );
+Vue.use( MdToolbar );
 
 Vue.config.productionTip = false
 
